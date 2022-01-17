ad2e12dbb98fe9b2870a359d79a1cb4ad84af112
unknown <sudharsanam.balaji@gmail.com>
2022-01-17 23:11:24 +0530

    added t_gamers_credits

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS t_games;
  
 CREATE TABLE t_games (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
+  games_id INT AUTO_INCREMENT  PRIMARY KEY,
   name VARCHAR(250) NOT NULL,
   description VARCHAR(500)
 );
@@ -9,10 +9,23 @@ CREATE TABLE t_games (
 DROP TABLE IF EXISTS t_gamers;
 
 CREATE TABLE t_gamers (
-  id INT AUTO_INCREMENT,
+  gamers_id INT AUTO_INCREMENT,
   name VARCHAR(250) NOT NULL,
   nick_name VARCHAR(250) ,
   gender VARCHAR(250) check(gender in ('FEMALE', 'MALE', 'TRANS', 'PERSONAL') ),
   email VARCHAR(250) PRIMARY KEY,
   geo VARCHAR(25) check(geo in ('EUROPE', 'ASIA', 'NORTH_AMERICA', 'SOUTH_AMERICA', 'AFRICA', 'AUSTRALIA', 'ANTARTICA') )
 );
+
+DROP TABLE IF EXISTS t_gamers_credits;
+
+CREATE TABLE t_gamers_credits (
+  gamers_credit_id INT AUTO_INCREMENT PRIMARY KEY,
+  level VARCHAR(250) check(level in ('NOOB', 'PRO', 'INVINSIBLE') ),
+  credits NUMERIC NOT NULL DEFAULT 0,
+  games_id INT,
+  foreign key (games_id) references t_games(games_id),
+  email VARCHAR(250),
+  foreign key (email) references t_gamers(email)
+)
+
