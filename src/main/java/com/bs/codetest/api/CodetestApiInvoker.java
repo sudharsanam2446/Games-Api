ca8aea68d94f22ac0026178e75b38ce2fe9f3b2f
unknown <sudharsanam.balaji@gmail.com>
2022-01-17 11:37:52 +0530

    removed unwanted stub

--- src/main/java/com/bs/codetest/api/CodetestApiInvoker.java
+++ src/main/java/com/bs/codetest/api/CodetestApiInvoker.java
@@ -11,31 +11,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.bs.codetest.api", "org.openapitools.configuration"})
 public class CodetestApiInvoker {
 
-   /* @Override
-    public void run(String... arg0) throws Exception {
-        if (arg0.length > 0 && arg0[0].equals("exitcode")) {
-            throw new ExitException();
-        }
-    }*/
 
     public static void main(String[] args) throws Exception {
         new SpringApplication(CodetestApiInvoker.class).run(args);
     }
-
-    /*static class ExitException extends RuntimeException implements ExitCodeGenerator {
-
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public int getExitCode() {
-            return 10;
-        }
-
-    }*/
-
-    /*@Bean
-    public Module jsonNullableModule() {
-        return new JsonNullableModule();
-    }*/
 
 }
